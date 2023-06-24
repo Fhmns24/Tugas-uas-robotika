e16cb095eb79da16d5114cf8bd3d78aaab261079
Fhmns24 <fahminandas24@gmail.com>
2023-06-25 03:03:08 +0700

    first commit

---
+++ controllers/IRB4600/build/release/IRB4600.d
@@ -0,0 +1,6 @@
+build/release/IRB4600.o: IRB4600.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/keyboard.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
---
+++ controllers/UR3e/build/release/UR3e.d
@@ -0,0 +1,6 @@
+build/release/UR3e.o: UR3e.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/keyboard.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h
